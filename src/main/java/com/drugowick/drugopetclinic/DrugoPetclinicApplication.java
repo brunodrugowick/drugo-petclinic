35d7a129c4e8772b28e2eff23a76b5a026970e1c
brunodrugowick <bruno.drugowick@gmail.com>
2019-02-27 23:44:39 -0300

    Initial project from Initializr.

---
+++ src/main/java/com/drugowick/drugopetclinic/DrugoPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.drugowick.drugopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugoPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugoPetclinicApplication.class, args);
+    }
+
+}
