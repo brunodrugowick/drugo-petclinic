c7a5352b571f5b2cd0be98cb010f953546bb120a
Bruno Drugowick Muniz <bruno.drugowick@gmail.com>
2019-03-05 21:34:57 -0300

    Adding BaseEntity. Closes #10.

--- petclinic-data/src/main/java/com/drugowick/drugopetclinic/model/Person.java
+++ petclinic-data/src/main/java/com/drugowick/drugopetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.drugowick.drugopetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- petclinic-data/src/main/java/com/drugowick/drugopetclinic/model/PetType.java
+++ petclinic-data/src/main/java/com/drugowick/drugopetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.drugowick.drugopetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
