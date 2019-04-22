a3336b7580bf38abf9b43c31c211dd5b4ae437ec
Bruno Drugowick Muniz <bruno.drugowick@gmail.com>
2019-04-22 01:17:44 -0300

    Closes #42.
    Also adding the profile definition to application.properties.

---
+++ petclinic-data/src/main/java/com/drugowick/drugopetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,48 @@
+package com.drugowick.drugopetclinic.services.springdatajpa;
+
+import com.drugowick.drugopetclinic.model.PetType;
+import com.drugowick.drugopetclinic.repositories.PetTypeRepository;
+import com.drugowick.drugopetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("externalDatabase")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> pets = new HashSet<>();
+        petTypeRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
