531e8b8bd46575ac6263c93cb64d3b622871294c
Bruno Drugowick Muniz <bruno.drugowick@gmail.com>
2019-04-22 23:48:51 -0300

    Closes #46.

---
+++ petclinic-data/src/main/java/com/drugowick/drugopetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.drugowick.drugopetclinic.services;
+
+import com.drugowick.drugopetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>  {
+}
---
+++ petclinic-data/src/main/java/com/drugowick/drugopetclinic/services/map/VisitMapService.java
@@ -0,0 +1,39 @@
+package com.drugowick.drugopetclinic.services.map;
+
+import com.drugowick.drugopetclinic.model.Visit;
+import com.drugowick.drugopetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        // Verifies if the visit has what's necessary, pet and owner saved (has ID), in order to be then saved.
+        if (visit.getPet() == null || visit.getPet().getOwner() == null
+            || visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Visits must have an Owner and a Pet.");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
