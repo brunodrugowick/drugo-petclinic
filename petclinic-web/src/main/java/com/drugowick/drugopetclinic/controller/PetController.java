e54e9e654dc1e9478d3dc6f69bce31e0311d34fa
Bruno Drugowick <bruno.drugowick@gmail.com>
2019-11-20 00:03:36 -0300

    Model attributes and data binder rules for PetController. #56

--- petclinic-web/src/main/java/com/drugowick/drugopetclinic/controller/PetController.java
+++ petclinic-web/src/main/java/com/drugowick/drugopetclinic/controller/PetController.java
@@ -1,18 +1,47 @@
 package com.drugowick.drugopetclinic.controller;
 
+import com.drugowick.drugopetclinic.model.Owner;
+import com.drugowick.drugopetclinic.model.PetType;
+import com.drugowick.drugopetclinic.services.OwnerService;
 import com.drugowick.drugopetclinic.services.PetService;
+import com.drugowick.drugopetclinic.services.PetTypeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @RequestMapping("/pets")
 @Controller
 public class PetController {
 
     private final PetService petService;
+    private final PetTypeService petTypeService;
+    private final OwnerService ownerService;
 
-    public PetController(PetService petService) {
+    public PetController(PetService petService, PetTypeService petTypeService, OwnerService ownerService) {
         this.petService = petService;
+        this.petTypeService = petTypeService;
+        this.ownerService = ownerService;
+    }
+
+    @ModelAttribute("types")
+    public Set<PetType> populatePetTypes() {
+        return this.petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
     }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
